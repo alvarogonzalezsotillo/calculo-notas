2585e0e3ea0bec7ba82fc692972f473c4d26c144
alvaro <alvarogonzalezsotillo@gmail.com>
2014-08-21 16:44:03 +0200

    Faltan los callbacks de creación y borrado de líneas

--- js/autocreacion.js
+++ js/autocreacion.js
@@ -6,48 +6,69 @@ var Autocreacion = (function(){
     }
     
     
-    function UnaAutocreacion(templateElem,fieldSelector,classToRemove){
-        this.templateElem = $(templateElem).clone();
-        this.parentElem = $(templateElem).parent();
-        this.fieldSelector = fieldSelector;
-        this.classToRemove = classToRemove;
+    function UnaAutocreacion(templateElem, fieldSelector, classToRemove, removeSelector, creationCallback, removeCallback){
+        var templateCopy = $(templateElem).clone();
+        var parentElem = $(templateElem).parent();
 
-        var self = this;
+        debug( "removeSelector:" + removeSelector );
         
-        this.fieldChanged = function(jqEvent){
-            var field = $(jqEvent.currentTarget);
-
-
-
-            var line = self.lineOf(field);
-
-            debug( "line:" + $(line).prop("class" ) );
-            
-            if( !line.hasClass(self.classToRemove) ){
-                debug( "Ya no tiene la clase:" + self.classToRemove );
-                return;
-            }
-
-            line.removeClass(self.classToRemove);
-            var newLine = $(self.templateElem).clone();
-            newLine = $(newLine);
-            $(fieldSelector,newLine).change( self.fieldChanged )
-
-            $(self.parentElem).append(newLine);
-
-        }
-
-        this.lineOf = function(field){
+        var lineOf = function(field){
             return $(field).parent();
         } 
         
-        $(fieldSelector,templateElem).change( this.fieldChanged )
+        var removeLine = function(line){
+            debug( "removeLine:" + line );
+            if( line.hasClass(classToRemove) ){
+                return;
+            }
+            $(line).remove();
+            if( removeCallback ){
+                removeCallback(line);
+            }
+        }
+        
+        var fieldChanged = function(jqEvent){
+            var field = $(jqEvent.currentTarget);
+
+            var line = lineOf(field);
+
+            debug( "line:" + $(line).prop("class" ) );
+            
+            if( !line.hasClass(classToRemove) ){
+                debug( "Ya no tiene la clase:" + classToRemove );
+                return;
+            }
+
+            line.removeClass(classToRemove);
+            var newLine = $(templateCopy).clone();
+            $(fieldSelector,newLine).change( fieldChanged )
+            $(removeSelector,newLine).click( function(){
+                debug( "borrando linea" );
+                removeLine( newLine );
+            });
+
+            $(parentElem).append(newLine);
+            
+            if( creationCallback ){
+                creationCallback(newLine);
+            }
+
+        }
+        
+        
+
+        
+        $(fieldSelector,templateElem).change( fieldChanged )
+        $(removeSelector,templateElem).click( function(){
+            debug( "borrando linea inicial" );
+            removeLine( templateElem );
+        });
         
     }
     
     return{
-        autocreacion: function(templateElem,fieldSelector,classToRemove){
-            return new UnaAutocreacion(templateElem,fieldSelector,classToRemove);
+        autocreacion: function(templateElem,fieldSelector,classToRemove,removeSelector){
+            UnaAutocreacion(templateElem,fieldSelector,classToRemove,removeSelector);
         }
     };
     
